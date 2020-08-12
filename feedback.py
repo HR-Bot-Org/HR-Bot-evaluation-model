bcd7c5eaf786742d7e8c1fc7ac1232c315ff0a53
AlyMagged <alimaged98@gmail.com>
2020-08-12 07:09:51 +0200

    add dictionary as result

--- feedback.py
+++ feedback.py
@@ -4,49 +4,35 @@ from googlesearch import search
 
 class Parser:
 
-    def __init__(self, skills):
-        self.skills = skills
+    def __init__(self, skill):
+        self.skill = skill
 
     def parse(self, sign):
-        sentences = []
-        for skill in self.skills:
-            sentences.append(skill['name'].split())
         
-        search_keys = []
+        sentence = self.skill.split()
         search_key = ''
+        for i in range(len(sentence)-1):
+            search_key += sentence[i]
+            search_key += sign
+        # print(sentence[-1])    
+        search_key += sentence[-1]
 
-        for sentence in sentences:
-            for i in range(len(sentences)-1):
-                search_key += sentence[i]
-                search_key += sign
-            search_key += sentence[-1]
-            search_keys.append(search_key)
-            search_key = ''
-
-        return search_keys
+        return search_key
 
 class FeedBack:
     
-    def __init__(self, data):
-        self.skills = data['skills']
-        self.question = data['question']
-        self.token = data['token']
+    def __init__(self, skills):
+        self.skills = skills
 
         
 
     #Done
-    def wikipediaSearch(self):
-        result = []
+    def wikipediaSearch(self, skill):
         wiki_wiki = wikipediaapi.Wikipedia('en')
-        page_question = wiki_wiki.page(self.question)
-        skills = self.skills
+        temp = wiki_wiki.page(skill)
+        summary = temp.summary
         
-        for skill in skills:
-            x = wiki_wiki.page(skill['name'])
-            result.append(x.summary)
-
-        result.append(page_question.summary)
-        return result
+        return summary
     
     #Done
     def googleSearch(self):
@@ -55,89 +41,115 @@ class FeedBack:
         result = []
 
         for skill in skills:
-            query = 'site:' + skill['site'] + ' ' + question + ' + ' + skill['name']
+            query = question + ' + ' + skill['name']
             for j in search(query, tld="com", num=10, stop=1, pause=2): 
                 result.append(str(j)) 
 
         return result
 
     #Done
-    def courseraSearch(self):
-        p = Parser(self.skills)
-        search_keys = p.parse('%20')
-        links = []
-        result = []
-        for search_key in search_keys:
-            link = 'https://www.coursera.org/search?query=' + search_key
-            links.append(link)
+    def courseraSearch(self, skill):
+        p = Parser(skill)
+        search_key = p.parse('%20')
+        link = 'https://www.coursera.org/search?query=' + search_key
+        # links = []
+        # result = []
+        # for search_key in search_keys:
+        #     link = 'https://www.coursera.org/search?query=' + search_key
+        #     links.append(link)
         
-        for link in links :
-            result.append(link)
+        # for link in links :
+        #     result.append(link)
 
-        return result
+        return link
 
     #Done
-    def udemySearch(self):
-        p = Parser(self.skills)
-        search_keys = p.parse('+')
-        links = []
-        result = []
-        for search_key in search_keys:
-            link = 'https://www.udemy.com/courses/search/?q=' + search_key
-            links.append(link)
+    def udemySearch(self, skill):
+        p = Parser(skill)
+        search_key = p.parse('+')
+        link = 'https://www.udemy.com/courses/search/?q=' + search_key
+        # links = []
+        # result = []
+        # for search_key in search_keys:
+        #     link = 'https://www.udemy.com/courses/search/?q=' + search_key
+        #     links.append(link)
         
-        for link in links :
-            result.append(link)
+        # for link in links :
+        #     result.append(link)
 
-        return result
+        return link
     
     #Done
-    def pluralsightSearch(self):
-        p = Parser(self.skills)
-        search_keys = p.parse('%20')
-        links = []
-        result = []
-        for search_key in search_keys:
-            link = 'https://www.pluralsight.com/search?q=' + search_key
-            links.append(link)
+    def pluralsightSearch(self, skill):
+        p = Parser(skill)
+        search_key = p.parse('%20')
+        link = 'https://www.pluralsight.com/search?q=' + search_key
+        # links = []
+        # result = []
+        # for search_key in search_keys:
+        #     link = 'https://www.pluralsight.com/search?q=' + search_key
+        #     links.append(link)
         
-        for link in links :
-            result.append(link)
+        # for link in links :
+        #     result.append(link)
 
-        return result
+        return link
 
     #Done
-    def lyndaSearch(self):
-        p = Parser(self.skills)
-        search_keys = p.parse('%20')
-        links = []
-        result = []
-        for search_key in search_keys:
-            link = 'https://www.lynda.com/search?q=' + search_key
-            links.append(link)
+    def lyndaSearch(self, skill):
+        p = Parser(skill)
+        search_key = p.parse('%20')
+        link = 'https://www.lynda.com/search?q=' + search_key
+        # links = []
+        # result = []
+        # for search_key in search_keys:
+        #     link = 'https://www.lynda.com/search?q=' + search_key
+        #     links.append(link)
         
-        for link in links :
-            result.append(link)
+        # for link in links :
+        #     result.append(link)
 
-        return result
+        return link
 
-    def search(self):
-        token = self.token 
-        if token == "hr_bot_2019_2020":    
-            result = []
-            result.append(self.wikipediaSearch())
-            result.append(self.googleSearch())
-            result.append(self.courseraSearch())
-            result.append(self.udemySearch())
-            result.append(self.pluralsightSearch())
-            result.append(self.lyndaSearch())
-            
-            return result
+    def create_a_skill_summary(self, skill):
+        summary = self.wikipediaSearch(skill)
+        return summary
+    
+    def create_a_skill_links(self, skill):
+        links = []
+        links.append(self.courseraSearch(skill))
+        links.append(self.udemySearch(skill))
+        links.append(self.pluralsightSearch(skill))
+        links.append(self.lyndaSearch(skill))
 
-        else:
-            return None
+        
+        # result = {
+        #     "skill1":
+        #     {
+        #     "summary": "From Weki",
+        #     "links": [
+        #         "https://...", "https://...", "https://...", "https://..."
+        #     ]
+        # }
+
+        # }
+             
+        return links
+    def create_feedback(self):
+
+        dictionary = {
+
+        }
+        for skill in self.skills:
+            dictionary[skill] = {}
+            dictionary[skill]["summary"] = self.create_a_skill_summary(skill)
+            dictionary[skill]["links"] = self.create_a_skill_links(skill)
+        return dictionary
 
 # p = Parser('machine learning andrew eg')
 # print(    p.parseForCoursera())
 if __name__ == "__main__":
-    pass
+    # p = Parser("data base")
+    # print(p.parse('+'))
+    f = FeedBack(["data base", "machine learning"])
+    print(f.create_feedback())
